29fdf659074eb214896b396060600a1a77c95741
TechyTR <hasanakmercan05@gmail.com>
2026-08-30 22:00:15 +0300

    Update bottom_nav_bar.dart

--- lib/widgets/bottom_nav_bar.dart
+++ lib/widgets/bottom_nav_bar.dart
@@ -2,7 +2,9 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
-class BottomNavBar extends StatefulWidget {
+import '../theme/app_theme.dart';
+
+class BottomNavBar extends StatelessWidget {
   final int currentIndex;
   final ValueChanged<int> onDestinationSelected;
 
@@ -13,332 +15,193 @@ class BottomNavBar extends StatefulWidget {
   });
 
   @override
-  State<BottomNavBar> createState() => _BottomNavBarState();
-}
-
-class _BottomNavBarState extends State<BottomNavBar>
-    with SingleTickerProviderStateMixin {
-  late final AnimationController _controller;
-
-  int _previousIndex = 0;
-
-  final List<IconData> _icons = const [
-    Icons.memory_outlined,
-    Icons.note_outlined,
-    Icons.info_outline,
-  ];
-
-  final List<IconData> _selectedIcons = const [
-    Icons.memory,
-    Icons.note,
-    Icons.info,
-  ];
-
-  final List<String> _labels = const [
-    'System',
-    'Notes',
-    'App',
-  ];
-
-  @override
-  void initState() {
-    super.initState();
-
-    _previousIndex = widget.currentIndex;
-
-    _controller = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 350),
-    );
-  }
-
-  @override
-  void didUpdateWidget(covariant BottomNavBar oldWidget) {
-    super.didUpdateWidget(oldWidget);
-
-    if (oldWidget.currentIndex != widget.currentIndex) {
-      _previousIndex = oldWidget.currentIndex;
-      _controller.forward(from: 0);
-    }
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    final colorScheme = theme.colorScheme;
+    final scheme = theme.colorScheme;
+
+    final isLiquidGlass =
+        Theme.of(context).scaffoldBackgroundColor ==
+                Colors.white ||
+            Theme.of(context).scaffoldBackgroundColor ==
+                Colors.black;
+
+    if (!isLiquidGlass) {
+      return NavigationBar(
+        selectedIndex: currentIndex,
+        onDestinationSelected:
+            onDestinationSelected,
+        destinations: const [
+          NavigationDestination(
+            icon: Icon(Icons.memory_outlined),
+            selectedIcon: Icon(Icons.memory),
+            label: 'System',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.note_outlined),
+            selectedIcon: Icon(Icons.note),
+            label: 'Notes',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.info_outline),
+            selectedIcon: Icon(Icons.info),
+            label: 'App',
+          ),
+        ],
+      );
+    }
 
     return SafeArea(
-      minimum: const EdgeInsets.fromLTRB(16, 8, 16, 12),
+      minimum: const EdgeInsets.fromLTRB(
+        16,
+        8,
+        16,
+        12,
+      ),
       child: ClipRRect(
         borderRadius: BorderRadius.circular(28),
         child: BackdropFilter(
           filter: ImageFilter.blur(
-            sigmaX: 18,
-            sigmaY: 18,
+            sigmaX: 20,
+            sigmaY: 20,
           ),
           child: Container(
             height: 72,
             decoration: BoxDecoration(
-              color: colorScheme.surface.withOpacity(0.72),
-              borderRadius: BorderRadius.circular(28),
+              color: theme.brightness ==
+                      Brightness.light
+                  ? Colors.white.withOpacity(0.55)
+                  : Colors.black.withOpacity(0.55),
+              borderRadius:
+                  BorderRadius.circular(28),
               border: Border.all(
-                color: Colors.white.withOpacity(0.12),
+                color:
+                    Colors.white.withOpacity(0.20),
               ),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.18),
-                  blurRadius: 24,
-                  spreadRadius: 2,
+            ),
+            child: Row(
+              children: [
+                _item(
+                  context,
+                  Icons.memory_outlined,
+                  Icons.memory,
+                  'System',
+                  0,
+                  scheme.primary,
+                ),
+                _item(
+                  context,
+                  Icons.note_outlined,
+                  Icons.note,
+                  'Notes',
+                  1,
+                  scheme.primary,
+                ),
+                _item(
+                  context,
+                  Icons.info_outline,
+                  Icons.info,
+                  'App',
+                  2,
+                  scheme.primary,
                 ),
               ],
             ),
-            child: LayoutBuilder(
-              builder: (context, constraints) {
-                final itemWidth =
-                    constraints.maxWidth / _icons.length;
+          ),
+        ),
+      ),
+    );
+  }
 
-                return Stack(
-                  children: [
-                    AnimatedBuilder(
-                      animation: _controller,
-                      builder: (context, child) {
-                        final start =
-                            _previousIndex * itemWidth;
+  Widget _item(
+    BuildContext context,
+    IconData icon,
+    IconData selectedIcon,
+    String label,
+    int index,
+    Color accent,
+  ) {
+    final selected =
+        currentIndex == index;
 
-                        final end =
-                            widget.currentIndex * itemWidth;
-
-                        final progress =
-                            Curves.easeOutCubic.transform(
-                          _controller.value,
-                        );
-
-                        final position = lerpDouble(
-                          start,
-                          end,
-                          progress,
-                        )!;
-
-                        return Positioned(
-                          left: position + 6,
-                          top: 6,
-                          width: itemWidth - 12,
-                          height: 60,
-                          child: _GlassSelection(
-                            color: colorScheme.primary,
-                          ),
-                        );
-                      },
-                    ),
-                    Row(
-                      children: List.generate(
-                        _icons.length,
-                        (index) {
-                          final selected =
-                              widget.currentIndex == index;
-
-                          return Expanded(
-                            child: _NavItem(
-                              icon: selected
-                                  ? _selectedIcons[index]
-                                  : _icons[index],
-                              label: _labels[index],
-                              selected: selected,
-                              color: colorScheme.primary,
-                              onTap: () {
-                                widget.onDestinationSelected(
-                                  index,
-                                );
-                              },
-                            ),
-                          );
-                        },
+    return Expanded(
+      child: GestureDetector(
+        onTap: () {
+          onDestinationSelected(index);
+        },
+        behavior: HitTestBehavior.opaque,
+        child: Padding(
+          padding: const EdgeInsets.all(6),
+          child: AnimatedContainer(
+            duration:
+                const Duration(milliseconds: 300),
+            curve: Curves.easeOutCubic,
+            decoration: BoxDecoration(
+              color: selected
+                  ? accent.withOpacity(0.22)
+                  : Colors.transparent,
+              borderRadius:
+                  BorderRadius.circular(22),
+              border: selected
+                  ? Border.all(
+                      color:
+                          Colors.white.withOpacity(
+                        0.22,
                       ),
-                    ),
-                  ],
-                );
-              },
+                    )
+                  : null,
+              boxShadow: selected
+                  ? [
+                      BoxShadow(
+                        color:
+                            accent.withOpacity(0.16),
+                        blurRadius: 14,
+                        spreadRadius: 1,
+                      ),
+                    ]
+                  : null,
             ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class _GlassSelection extends StatelessWidget {
-  final Color color;
-
-  const _GlassSelection({
-    required this.color,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(22),
-      child: BackdropFilter(
-        filter: ImageFilter.blur(
-          sigmaX: 8,
-          sigmaY: 8,
-        ),
-        child: Container(
-          decoration: BoxDecoration(
-            color: color.withOpacity(0.22),
-            borderRadius: BorderRadius.circular(22),
-            border: Border.all(
-              color: Colors.white.withOpacity(0.20),
-            ),
-            boxShadow: [
-              BoxShadow(
-                color: color.withOpacity(0.20),
-                blurRadius: 16,
-                spreadRadius: 1,
-              ),
-            ],
-          ),
-          child: Stack(
-            children: [
-              Positioned(
-                top: 0,
-                left: 12,
-                right: 12,
-                child: Container(
-                  height: 1,
-                  decoration: BoxDecoration(
-                    color: Colors.white.withOpacity(0.28),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class _NavItem extends StatefulWidget {
-  final IconData icon;
-  final String label;
-  final bool selected;
-  final Color color;
-  final VoidCallback onTap;
-
-  const _NavItem({
-    required this.icon,
-    required this.label,
-    required this.selected,
-    required this.color,
-    required this.onTap,
-  });
-
-  @override
-  State<_NavItem> createState() => _NavItemState();
-}
-
-class _NavItemState extends State<_NavItem>
-    with SingleTickerProviderStateMixin {
-  late final AnimationController _animationController;
-
-  @override
-  void initState() {
-    super.initState();
-
-    _animationController = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 250),
-      value: widget.selected ? 1 : 0,
-    );
-  }
-
-  @override
-  void didUpdateWidget(covariant _NavItem oldWidget) {
-    super.didUpdateWidget(oldWidget);
-
-    if (oldWidget.selected != widget.selected) {
-      if (widget.selected) {
-        _animationController.forward();
-      } else {
-        _animationController.reverse();
-      }
-    }
-  }
-
-  @override
-  void dispose() {
-    _animationController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final scheme = Theme.of(context).colorScheme;
-
-    return InkWell(
-      onTap: widget.onTap,
-      borderRadius: BorderRadius.circular(22),
-      child: SizedBox(
-        height: 72,
-        child: AnimatedBuilder(
-          animation: _animationController,
-          builder: (context, child) {
-            final progress = Curves.easeOutBack.transform(
-              _animationController.value,
-            );
-
-            final scale = lerpDouble(
-              0.88,
-              1.08,
-              progress,
-            )!;
-
-            final opacity = lerpDouble(
-              0.55,
-              1.0,
-              _animationController.value,
-            )!;
-
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+            child: Column(
+              mainAxisAlignment:
+                  MainAxisAlignment.center,
               children: [
-                Transform.scale(
-                  scale: scale,
-                  child: Opacity(
-                    opacity: opacity,
-                    child: Icon(
-                      widget.icon,
-                      size: 25,
-                      color: widget.selected
-                          ? widget.color
-                          : scheme.onSurfaceVariant,
-                    ),
+                AnimatedScale(
+                  scale: selected ? 1.08 : 1.0,
+                  duration: const Duration(
+                    milliseconds: 250,
+                  ),
+                  child: Icon(
+                    selected
+                        ? selectedIcon
+                        : icon,
+                    color: selected
+                        ? accent
+                        : Theme.of(context)
+                            .colorScheme
+                            .onSurfaceVariant,
+                    size: 25,
                   ),
                 ),
                 const SizedBox(height: 4),
                 AnimatedDefaultTextStyle(
-                  duration: const Duration(milliseconds: 200),
+                  duration: const Duration(
+                    milliseconds: 200,
+                  ),
                   style: TextStyle(
-                    fontSize: widget.selected ? 12 : 11,
-                    fontWeight: widget.selected
+                    fontSize: selected ? 12 : 11,
+                    fontWeight: selected
                         ? FontWeight.w700
                         : FontWeight.w500,
-                    color: widget.selected
-                        ? widget.color
-                        : scheme.onSurfaceVariant,
+                    color: selected
+                        ? accent
+                        : Theme.of(context)
+                            .colorScheme
+                            .onSurfaceVariant,
                   ),
-                  child: Text(widget.label),
+                  child: Text(label),
                 ),
               ],
-            );
-          },
+            ),
+          ),
         ),
       ),
     );
