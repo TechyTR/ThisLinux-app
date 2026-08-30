1d1d23f26fbfba4540a9106fec00612727a1b36e
TechyTR <hasanakmercan05@gmail.com>
2026-08-30 21:03:20 +0300

    Update bottom_nav_bar.dart

--- lib/widgets/bottom_nav_bar.dart
+++ lib/widgets/bottom_nav_bar.dart
@@ -1,6 +1,8 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
-class BottomNavBar extends StatelessWidget {
+class BottomNavBar extends StatefulWidget {
   final int currentIndex;
   final ValueChanged<int> onDestinationSelected;
 
@@ -11,27 +13,290 @@ class BottomNavBar extends StatelessWidget {
   });
 
   @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  int _previousIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _previousIndex = widget.currentIndex;
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 350),
+      curve: Curves.easeOutCubic,
+    );
+  }
+
+  @override
+  void didUpdateWidget(covariant BottomNavBar oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    if (oldWidget.currentIndex != widget.currentIndex) {
+      _previousIndex = oldWidget.currentIndex;
+      _controller.forward(from: 0);
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  final List<IconData> _icons = const [
+    Icons.memory_outlined,
+    Icons.note_outlined,
+    Icons.info_outline,
+  ];
+
+  final List<IconData> _selectedIcons = const [
+    Icons.memory,
+    Icons.note,
+    Icons.info,
+  ];
+
+  final List<String> _labels = const [
+    'System',
+    'Notes',
+    'App',
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return NavigationBar(
-      selectedIndex: currentIndex,
-      onDestinationSelected: onDestinationSelected,
-      destinations: const [
-        NavigationDestination(
-          icon: Icon(Icons.memory_outlined),
-          selectedIcon: Icon(Icons.memory),
-          label: 'System',
+    final theme = Theme.of(context);
+    final colorScheme = theme.colorScheme;
+
+    return SafeArea(
+      minimum: const EdgeInsets.fromLTRB(16, 8, 16, 12),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(28),
+        child: BackdropFilter(
+          filter: ImageFilter.blur(
+            sigmaX: 18,
+            sigmaY: 18,
+          ),
+          child: Container(
+            height: 72,
+            decoration: BoxDecoration(
+              color: colorScheme.surface.withValues(alpha: 0.72),
+              borderRadius: BorderRadius.circular(28),
+              border: Border.all(
+                color: Colors.white.withValues(alpha: 0.12),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withValues(alpha: 0.18),
+                  blurRadius: 24,
+                  spreadRadius: 2,
+                ),
+              ],
+            ),
+            child: LayoutBuilder(
+              builder: (context, constraints) {
+                final itemWidth = constraints.maxWidth / _icons.length;
+
+                return Stack(
+                  children: [
+                    AnimatedBuilder(
+                      animation: _controller,
+                      builder: (context, child) {
+                        final start = _previousIndex * itemWidth;
+                        final end = widget.currentIndex * itemWidth;
+
+                        final position = lerpDouble(
+                          start,
+                          end,
+                          Curves.easeOutCubic.transform(
+                            _controller.value,
+                          ),
+                        )!;
+
+                        return Positioned(
+                          left: position + 6,
+                          top: 6,
+                          width: itemWidth - 12,
+                          height: 60,
+                          child: _GlassSelection(
+                            color: colorScheme.primary,
+                          ),
+                        );
+                      },
+                    ),
+
+                    Row(
+                      children: List.generate(
+                        _icons.length,
+                        (index) {
+                          final selected =
+                              widget.currentIndex == index;
+
+                          return Expanded(
+                            child: _NavItem(
+                              icon: selected
+                                  ? _selectedIcons[index]
+                                  : _icons[index],
+                              label: _labels[index],
+                              selected: selected,
+                              color: colorScheme.primary,
+                              onTap: () {
+                                widget.onDestinationSelected(index);
+                              },
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                );
+              },
+            ),
+          ),
         ),
-        NavigationDestination(
-          icon: Icon(Icons.note_outlined),
-          selectedIcon: Icon(Icons.note),
-          label: 'Notes',
-        ),
-        NavigationDestination(
-          icon: Icon(Icons.info_outline),
-          selectedIcon: Icon(Icons.info),
-          label: 'App',
-        ),
-      ],
+      ),
     );
   }
 }
+
+class _GlassSelection extends StatelessWidget {
+  final Color color;
+
+  const _GlassSelection({
+    required this.color,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(22),
+      child: BackdropFilter(
+        filter: ImageFilter.blur(
+          sigmaX: 8,
+          sigmaY: 8,
+        ),
+        child: Container(
+          decoration: BoxDecoration(
+            color: color.withValues(alpha: 0.22),
+            borderRadius: BorderRadius.circular(22),
+            border: Border.all(
+              color: Colors.white.withValues(alpha: 0.20),
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: color.withValues(alpha: 0.20),
+                blurRadius: 16,
+                spreadRadius: 1,
+              ),
+            ],
+          ),
+          child: Stack(
+            children: [
+              Positioned(
+                top: 0,
+                left: 12,
+                right: 12,
+                child: Container(
+                  height: 1,
+                  decoration: BoxDecoration(
+                    color: Colors.white.withValues(alpha: 0.28),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _NavItem extends StatefulWidget {
+  final IconData icon;
+  final String label;
+  final bool selected;
+  final Color color;
+  final VoidCallback onTap;
+
+  const _NavItem({
+    required this.icon,
+    required this.label,
+    required this.selected,
+    required this.color,
+    required this.onTap,
+  });
+
+  @override
+  State<_NavItem> createState() => _NavItemState();
+}
+
+class _NavItemState extends State<_NavItem>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 250),
+    );
+
+    if (widget.selected) {
+      _animationController.value = 1;
+    }
+  }
+
+  @override
+  void didUpdateWidget(covariant _NavItem oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    if (oldWidget.selected != widget.selected) {
+      if (widget.selected) {
+        _animationController.forward();
+      } else {
+        _animationController.reverse();
+      }
+    }
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: widget.onTap,
+      borderRadius: BorderRadius.circular(22),
+      child: SizedBox(
+        height: 72,
+        child: AnimatedBuilder(
+          animation: _animationController,
+          builder: (context, child) {
+            final scale = lerpDouble(
+              0.88,
+              1.08,
+              Curves.easeOutBack.transform(
+                _animationController.value,
+              ),
+            )!;
+
+            final opacity = lerpDouble(
+              0.55,
+              1.0,
+              _animationController.value,
+            )!;
+
+            return Column(
+             
