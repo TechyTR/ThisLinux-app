eeb8e9c9e5d721e56d850e7c1cf105a211685a26
TechyTR <hasanakmercan05@gmail.com>
2026-08-30 21:01:25 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import 'pages/boot_screen.dart';
-import 'services/preferences_service.dart';
 import 'theme/app_theme.dart';
 
 void main() {
@@ -19,42 +18,20 @@ class _ThisLinuxAppState extends State<ThisLinuxApp> {
   AppThemeColor _selectedTheme = AppThemeColor.purple;
 
   @override
-  void initState() {
-    super.initState();
-    _loadTheme();
-  }
-
-  Future<void> _loadTheme() async {
-    final savedTheme = await PreferencesService.getThemeColor();
-
-    if (!mounted) return;
-
-    setState(() {
-      _selectedTheme = AppTheme.fromString(savedTheme);
-    });
-  }
-
-  Future<void> _changeTheme(AppThemeColor theme) async {
-    setState(() {
-      _selectedTheme = theme;
-    });
-
-    await PreferencesService.saveThemeColor(
-      AppTheme.toStringValue(theme),
-    );
-  }
-
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'ThisLinux',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.light(_selectedTheme),
       darkTheme: AppTheme.dark(_selectedTheme),
-      themeMode: ThemeMode.system,
+      themeMode: ThemeMode.dark,
       home: BootScreen(
         selectedTheme: _selectedTheme,
-        onThemeChanged: _changeTheme,
+        onThemeChanged: (theme) {
+          setState(() {
+            _selectedTheme = theme;
+          });
+        },
       ),
     );
   }
